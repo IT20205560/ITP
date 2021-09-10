dcd4933bff930e4cc56aae669a401d8c6974627e
ACER <7hashini@gmail.com>
2021-09-10 21:04:42 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/navbar";
+import AdmitPatient from "./pages/AdmitPatient";
+import PatientList from "./pages/PatientList";
+import EditPatient from "./pages/EditPatient";
+import managePatient from "./components/PatientServices/managePatient";
+import ChargeCalculate from "./pages/ChargeCalculate";
+import ViewCharge from "./pages/viewCharge";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+ 
+    <Navbar />
+      <br/>
+      <Route path='/managePatient' component={managePatient} />
+      <Route path="/admitPatient" component={AdmitPatient} />
+      <Route path="/patientList" exact component={PatientList} />
+      <Route path="/edit/:id" component={EditPatient} />
+      <Route path="/chargeCalculate" component={ChargeCalculate} />
+      <Route path='/viewCharge' component={ViewCharge} />
+      
+    </Router>
   );
 }
 
